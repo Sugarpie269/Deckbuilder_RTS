526199328a0e16d7c6d46225e2be7351fa8f7e28
jjgaydon <2buildornot2build@gmail.com>
2021-05-13 14:46:39 -0700

    Created Unity Project

---
+++ DeckbuilderRTS/Assets/Resources/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+// Temporary citation = CaptainController.cs
+
+namespace DeckbuilderRTS
+{
+    public class PlayerController : MonoBehavior
+    {
+        // private Inventory PlayerInventory;
+        // private IPlayerCommand MoveUp;
+        // private IPlayerCommand MoveDown;
+        // private IPlayerCommand MoveLeft;
+        // private IPlayerCommand MoveRight;
+
+        // The start function will initialize our member variables.
+        void Start()
+        {
+            // this.PlayerInventory = this.gameObject.GetComponent<Inventory>();
+            // this.MoveUp = ScriptableObject.CreateInstance(MovePlayerUp);
+            // this.MoveDown = ScriptableObject.CreateInstance(MovePlayerDown);
+            // this.MoveDown = ScriptableObject.CreateInstance(MovePlayerLeft);
+            // this.MoveDown = ScriptableObject.CreateInstance(MovePlayerRight);
+        }
+
+        // Navya, this is all you!
+        void ProcessInput()
+        {
+
+        }
+
+        void Update()
+        {
+            this.ProcessInput();
+        }
+    }
+}
