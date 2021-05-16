cbf872ac6baa4f8e1ef4670a4ed17ad7a08e0e0d
amyhuycu <amycu@ucdavis.edu>
2021-05-15 17:06:12 -0700

    Some code for Moving player up/down/left/right

--- DeckbuilderRTS/Assets/Resources/Scripts/MovePlayerDownCommand.cs
+++ DeckbuilderRTS/Assets/Resources/Scripts/MovePlayerDownCommand.cs
@@ -8,9 +8,26 @@ namespace DeckbuilderRTS
 {
     public class MovePlayerDownCommand : ScriptableObject, IPlayerCommand
     {
+
+        // FIXME: Add option for speed changes?
+        private float Speed = 5.0f;
+
         public void Execute(GameObject gameObject)
         {
-
+            var rigidBody = gameObject.GetComponent<Rigidbody2D>();
+            if (rigidBody != null)
+            {
+                if (gameObject.GetComponent<SpriteRenderer>().flipX == true) {
+                    // Move Up Facing Left
+                    rigidBody.velocity = new Vector2(rigidBody.velocity.x, -this.Speed);
+                    gameObject.GetComponent<SpriteRenderer>().flipX = true;
+                }
+                else {
+                    // Move Up Facing Right
+                    rigidBody.velocity = new Vector2(rigidBody.velocity.x, -this.Speed);
+                    gameObject.GetComponent<SpriteRenderer>().flipX = false;
+                }
+            }
         }
     }
 }
--- DeckbuilderRTS/Assets/Resources/Scripts/MovePlayerLeftCommand.cs
+++ DeckbuilderRTS/Assets/Resources/Scripts/MovePlayerLeftCommand.cs
@@ -8,9 +8,18 @@ namespace DeckbuilderRTS
 {
     public class MovePlayerLeftCommand : ScriptableObject, IPlayerCommand
     {
+
+        // FIXME: Add option for speed changes?
+        private float Speed = 5.0f;
+        
         public void Execute(GameObject gameObject)
         {
-
+            var rigidBody = gameObject.GetComponent<Rigidbody2D>();
+            if (rigidBody != null)
+            {
+                rigidBody.velocity = new Vector2(-this.Speed, rigidBody.velocity.y);
+                gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            }
         }
     }
 }
