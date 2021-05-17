01c8be2682b014618e3c7ffc251dd61f5160c971
Lumsterling <74507628+Lumsterling@users.noreply.github.com>
2021-05-16 17:43:42 -0700

    Added placeholder UI

--- DeckbuilderRTS/Assets/Resources/Scripts/PlayerController.cs
+++ DeckbuilderRTS/Assets/Resources/Scripts/PlayerController.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+using TMPro;
 using DeckbuilderRTS;
 
 // Temporary citation = CaptainController.cs
@@ -8,6 +8,14 @@ namespace DeckbuilderRTS
 {
     public class PlayerController : MonoBehaviour
     {
+        // UI elements are as follows. ~Liam
+        public TextMeshProUGUI HealthText;
+        public GameObject VictoryText;
+        public GameObject GameOverText;
+        public GameObject LowHealthWarningText;
+        [SerializeField] private int PlayerCurrentHP;
+        [SerializeField] private int PlayerMaxHP;
+
         private Inventory PlayerInventory;
         private IPlayerCommand MoveUp;
         private IPlayerCommand MoveDown;
@@ -28,6 +36,57 @@ namespace DeckbuilderRTS
             this.PlayCard1 = ScriptableObject.CreateInstance<PlayerCard1Command>();
             this.PlayCard2 = ScriptableObject.CreateInstance<PlayerCard2Command>();
             this.PlayCard3 = ScriptableObject.CreateInstance<PlayerCard3Command>();
+
+            // Initialize UI elements, player health, and player deck + cards. ~Liam
+            this.VictoryText.SetActive(false);
+            this.LowHealthWarningText.SetActive(false);
+            this.GameOverText.SetActive(false);
+            this.SetHealthText();
+        }
+
+        // UI FUNCTION: Updates the health text when called. Also governs whether or not low HP or game over text is displayed. ~Liam
+        void SetHealthText()
+        {
+            // Update the HP text. ~Liam
+            this.HealthText.text = "HP: " + this.PlayerCurrentHP.ToString() + "/" + this.PlayerMaxHP.ToString();
+
+            // Warn the player if their health drops below 25% of the maximum. ~Liam
+            float percentHP = (float)this.PlayerCurrentHP / (float)this.PlayerMaxHP;
+            Debug.Log(percentHP);
+            if (percentHP <= 0.25f && this.PlayerCurrentHP != 0)
+            {
+                this.LowHealthWarningText.SetActive(true);
+            }
+            else
+            {
+                this.LowHealthWarningText.SetActive(false);
+            }
+            // If player's HP drops to 0, they lose, and the UI should reflect this. ~Liam
+            if (this.PlayerCurrentHP == 0)
+            {
+                this.GameOverText.SetActive(true);
+            }
+        }
+
+        // UI & PLAYER FUNCTION: Modifies the player's current health by the parameter value. Passed in integer can be positive (for healing) or negative (for damage). ~Liam
+        public void ModifyPlayerHealth(int amount)
+        {
+            // Add the parameter to the player's current health, keeping in mind max and min health values. ~Liam
+            if (this.PlayerCurrentHP + amount > this.PlayerMaxHP)
+            {
+                this.PlayerCurrentHP = this.PlayerMaxHP;
+            }
+            else if (this.PlayerCurrentHP + amount < 0)
+            {
+                this.PlayerCurrentHP = 0;
+            }
+            else
+            {
+                this.PlayerCurrentHP = this.PlayerCurrentHP + amount;
+            }
+
+            // Update the UI health value. ~Liam
+            this.SetHealthText();
         }
 
         // Navya, this is all you!
