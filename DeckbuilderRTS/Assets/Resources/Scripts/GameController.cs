accc0b62705c62bd5bfdad80c19b633d7e862840
jjgaydon <2buildornot2build@gmail.com>
2021-05-26 16:20:22 -0700

    Added miniboss swarmling summon zones

--- DeckbuilderRTS/Assets/Resources/Scripts/GameController.cs
+++ DeckbuilderRTS/Assets/Resources/Scripts/GameController.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+using System.Collections.ObjectModel;
 using DeckbuilderRTS;
 
 
@@ -9,13 +9,39 @@ namespace DeckbuilderRTS
     {
         [SerializeField] public Object FireballPrefab;
         [SerializeField] public Object WorkerPrefab;
+        private float GameTimerMinutes = 0.0f;
+        private float GameTimerSeconds = 0.0f;
+        [SerializeField] private float SummonSwarmlingDelay = 10.0f;
+        private float CurrentSummonSwarmlingDelay = 0.0f;
+
         // The start function will initialize our member variables.
         public void Start()
         {
+
         }
 
         public void Update()
         {
+            this.GameTimerSeconds += Time.deltaTime;
+            this.CurrentSummonSwarmlingDelay += Time.deltaTime;
+
+            // Reset the seconds clock if a minute should be added. ~Jackson
+            if (this.GameTimerSeconds >= 60.0f)
+            {
+                this.GameTimerSeconds -= 60.0f;
+                this.GameTimerMinutes += 1.0f;
+            }
+
+            if (this.CurrentSummonSwarmlingDelay >= this.SummonSwarmlingDelay)
+            {
+                this.CurrentSummonSwarmlingDelay -= this.SummonSwarmlingDelay;
+                this.SummonNewSwarmling();
+            }
+        }
+
+        private void SummonNewSwarmling()
+        {
+
         }
 
         public ICard GenerateCardFireball()
