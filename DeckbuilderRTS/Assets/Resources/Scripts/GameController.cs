3acc6d51f438c5640dd1be03e02d0fe51b8b4769
jjgaydon <2buildornot2build@gmail.com>
2021-05-26 17:07:52 -0700

    Difficulty value increases with time

--- DeckbuilderRTS/Assets/Resources/Scripts/GameController.cs
+++ DeckbuilderRTS/Assets/Resources/Scripts/GameController.cs
@@ -11,12 +11,17 @@ namespace DeckbuilderRTS
         [SerializeField] public Object WorkerPrefab;
         [SerializeField] public Object SwarmlingPrefab;
         [SerializeField] private bool SummonSwarmlings = true;
+        [SerializeField] private bool IncreaseDifficulty = true;
         [SerializeField] public GameObject Miniboss1;
         [SerializeField] public GameObject Miniboss2;
         [SerializeField] public GameObject Miniboss3;
+
+        private int GameDifficulty = 1;
         private float GameTimerMinutes = 0.0f;
         private float GameTimerSeconds = 0.0f;
         [SerializeField] private float SummonSwarmlingDelay = 10.0f;
+        [SerializeField] private float IncreaseDifficultyDelay = 60.0f;
+        private float CurrentIncreaseDifficultyDelay = 0.0f;
         private float CurrentSummonSwarmlingDelay = 0.0f;
 
         // The start function will initialize our member variables.
@@ -29,12 +34,23 @@ namespace DeckbuilderRTS
         {
             this.GameTimerSeconds += Time.deltaTime;
             this.CurrentSummonSwarmlingDelay += Time.deltaTime;
+            this.CurrentIncreaseDifficultyDelay += Time.deltaTime;
 
             // Reset the seconds clock if a minute should be added. ~Jackson
             if (this.GameTimerSeconds >= 60.0f)
             {
                 this.GameTimerSeconds -= 60.0f;
                 this.GameTimerMinutes += 1.0f;
+            }
+
+            if (this.CurrentIncreaseDifficultyDelay >= this.IncreaseDifficultyDelay)
+            {
+                this.CurrentIncreaseDifficultyDelay -= this.IncreaseDifficultyDelay;
+
+                if (this.IncreaseDifficulty)
+                {
+                    this.GameDifficulty += 1;
+                }
             }
 
             if (this.CurrentSummonSwarmlingDelay >= this.SummonSwarmlingDelay)
