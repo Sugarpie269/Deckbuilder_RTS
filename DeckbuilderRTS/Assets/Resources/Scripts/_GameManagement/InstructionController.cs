488f95637affa160044acb768ba5c21c3b290cea
Lumsterling <74507628+Lumsterling@users.noreply.github.com>
2021-06-05 18:22:41 -0700

    WIP instruction scene

--- DeckbuilderRTS/Assets/Resources/Scripts/_GameManagement/InstructionController.cs
+++ DeckbuilderRTS/Assets/Resources/Scripts/_GameManagement/InstructionController.cs
@@ -8,10 +8,17 @@ public class InstructionController : MonoBehaviour
     //private AssetBundle LoadedAssetBundle;
     //private string[] ScenePaths;
     // Start is called before the first frame update
+    private GameObject[] AllTextInstructions;
+    private GameObject[] AllImageInstructions;
+    [SerializeField] private GameObject ControlsTitleText;
+    [SerializeField] private GameObject ControlText;
+    [SerializeField] private GameObject CameraWarningText;
     void Start()
     {
-        //LoadedAssetBundle = AssetBundle.LoadFromFile("Assets/scenes");
-        //ScenePaths = LoadedAssetBundle.GetAllScenePaths();
+        // Get all game objects with the Instructions Text tag, for deactivating quickly. ~Liam
+        this.AllTextInstructions = GameObject.FindGameObjectsWithTag("Instruction Text");
+        this.AllImageInstructions = GameObject.FindGameObjectsWithTag("Instruction Image");
+        this.DeactivateAllInfo();
     }
 
     public void ClickStartButton()
@@ -19,7 +26,38 @@ public class InstructionController : MonoBehaviour
         SceneManager.LoadScene("MainMap");
     }
 
+    public void ClickControlsButton()
+    {
+        // Deactivate any text on screen. ~Liam
+        this.DeactivateAllInfo();
 
+        // Activate the objects related to control information. ~Liam
+        this.ControlsTitleText.SetActive(true);
+        this.ControlText.SetActive(true);
+        this.CameraWarningText.SetActive(true);
+    }
+
+    public void ClickCardUsageButton()
+    {
+        // Deactivate any text on screen. ~Liam
+        this.DeactivateAllInfo();
+
+        // TODO: Activate objects related to card usage information. ~Liam
+    }
+    
+    private void DeactivateAllInfo()
+    {
+        // Deactivate each text object. ~Liam
+        foreach (GameObject gObject in this.AllTextInstructions)
+        {
+            gObject.SetActive(false);
+        }
+        // Deactivate each image object. ~Liam
+        foreach (GameObject gObject in this.AllImageInstructions)
+        {
+            gObject.SetActive(false);
+        }
+    }
 
     // Update is called once per frame
     void Update()
