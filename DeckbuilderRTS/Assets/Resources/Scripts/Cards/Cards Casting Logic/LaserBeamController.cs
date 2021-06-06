5eb189d67e5c851b028ae7b745b8ee26dec12718
Jarod H <jarodheng@gmail.com>
2021-06-06 13:21:36 -0700

    laserbeam functionality added, not yet tested

--- DeckbuilderRTS/Assets/Resources/Scripts/Cards/Cards Casting Logic/LaserBeamController.cs
+++ DeckbuilderRTS/Assets/Resources/Scripts/Cards/Cards Casting Logic/LaserBeamController.cs
@@ -8,12 +8,15 @@ namespace DeckbuilderRTS
     public class LaserBeamController : MonoBehaviour
     {
         private float Damage;
-        private Vector2 Velocity; // TODO remove velocity?
-
+        private float Delay; // delay in seconds before the bolt hurts things
+        private float DamageDelay;
+        private float DamageDelayCounter;
         public LaserBeamController()
         {
             this.Damage = 20.0f;
-            this.Velocity = new Vector2(20.0f, 0.0f);
+            this.Delay = 2.0f; // time before damage is dealt (charge up)
+            this.DamageDelay = 0.25f;
+            this.DamageDelayCounter = 0.0f;
         }
 
         // The start function will initialize our member variables.
@@ -21,53 +24,58 @@ namespace DeckbuilderRTS
         {
         }
 
-        public void SetAttributes(float damage, Vector2 velocity, float knockback)
+        public void SetAttributes(float damage, float delay)
         {
             this.Damage = damage;
-            this.Velocity = velocity;
+            this.Delay = delay;
         }
 
         public void Update()
         {
-            this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(this.Velocity.x, this.Velocity.y);
+            
+            //this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(this.Velocity.x, this.Velocity.y);
         }
 
-        private void OnCollisionEnter2D(Collision2D collision)
-        {
-            // If the fireball collides with a swarmling, the swarmling takes damage and knocks it back. Otherwise, the forcebolt is destroyed.
-            if (collision.collider.tag == "Swarmling")
-            {
-                collision.collider.GetComponent<SwarmlingController>().TakeDamage(this.Damage);
-                // TODO: Add knockback to it
-                Physics2D.IgnoreCollision(collision.collider, collision.otherCollider);
-                //GameObject.Destroy(this.gameObject);
-            }
-            /*else if (collision.collider.tag == "Player")
-            {
-                collision.collider.GetComponent<PlayerController>().TakeDamage(this.Damage);
-                GameObject.Destroy(this.gameObject);
-            }*/
-            else if (collision.collider.tag == "Boss")
-            {
-                collision.collider.GetComponent<BossController>().TakeDamage(this.Damage);
-                Physics2D.IgnoreCollision(collision.collider, collision.otherCollider);
-                //GameObject.Destroy(this.gameObject);
-            }
-            else if (collision.collider.tag == "Obstacle")
-            {
-                GameObject.Destroy(this.gameObject);
-            }
-            else
-            {
-                //Physics2D.IgnoreCollision(collision.collider, collision.otherCollider);
-            }
-
-        }
-
+        // if the things touching it are still in the collider, and the charge up has passed, deal damage
         private void OnCollisionStay2D(Collision2D collision)
         {
-            // TODO: if the things touching it are still in the collider, and the charge up has passed, deal damage
-            // destroy after a little bit of time to let animation finish
+            // ensure that damage is only dealt every DamageDelay (0.25) seconds
+            this.DamageDelayCounter += Time.deltaTime;
+            if (this.DamageDelayCounter >= this.DamageDelay)
+            {
+                this.DamageDelayCounter -= this.DamageDelay;
+                // If the fireball collides with a swarmling, the swarmling takes damage and knocks it back. Otherwise, the forcebolt is destroyed.
+                if (collision.collider.tag == "Swarmling")
+                {
+                    collision.collider.GetComponent<SwarmlingController>().TakeDamage(this.Damage);
+                    // TODO: Add knockback to it
+                    Physics2D.IgnoreCollision(collision.collider, collision.otherCollider);
+                    //GameObject.Destroy(this.gameObject);
+                }
+                /*else if (collision.collider.tag == "Player")
+                {
+                    collision.collider.GetComponent<PlayerController>().TakeDamage(this.Damage);
+                    GameObject.Destroy(this.gameObject);
+                }*/
+                else if (collision.collider.tag == "Boss")
+                {
+                    collision.collider.GetComponent<BossController>().TakeDamage(this.Damage);
+                    Physics2D.IgnoreCollision(collision.collider, collision.otherCollider);
+                    //GameObject.Destroy(this.gameObject);
+                }
+                else if (collision.collider.tag == "Obstacle")
+                {
+                    //GameObject.Destroy(collision.gameObject);
+                    collision.gameObject.SetActive(false);
+                }
+                else
+                {
+                    //Physics2D.IgnoreCollision(collision.collider, collision.otherCollider);
+                }
+            }
         }
+
+        
+            
     }
 }
