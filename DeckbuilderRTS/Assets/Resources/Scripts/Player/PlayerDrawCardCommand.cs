615eb4c481fb60c4e0503cfe00b7bdb84e4ffeb1
jjgaydon <2buildornot2build@gmail.com>
2021-06-09 14:12:27 -0700

    Revert "Removed unused files"
    This reverts commit b6a8693efc38cb964ab70a6b7a532344301f3d36.

---
+++ DeckbuilderRTS/Assets/Resources/Scripts/Player/PlayerDrawCardCommand.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DeckbuilderRTS;
+
+namespace DeckbuilderRTS
+{
+    public class PlayerDrawCardCommand : ScriptableObject, IPlayerCommand
+    {
+
+        // AUGH ~Liam
+        public void Execute(GameObject gameObject)
+        {
+
+        }
+    }
+}
